8a9310d93756e2520a243cab4d482e0923d099e1
mowenira89 <mowenira@gmail.com>
2025-10-13 20:44:09 -0400

    fixed initial mistake

--- buffs/shield.gd
+++ buffs/shield.gd
@@ -3,7 +3,7 @@ class_name Shield extends TimedBuff
 @export var shield_amount:int
 
 
-func alter_incoming_damage(amount:int,m:Move):
+func alter_incoming_damage(amount:int,m:Move,t):
 	if move.classes.has(DamageEffect.DAMAGES.Affliction):
 		return amount
 	var current_shield=shield_amount
